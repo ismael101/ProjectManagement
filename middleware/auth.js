9f854072f4ec1204ec07b54011c94db4e5d807e2
ismael101 <ismaelomermohamed@gmail.com>
2019-12-11 22:50:20 -0600

    added auth middleware and env file

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+module.exports = (req,res,next) => {
+    try{
+        const token = req.headers.authorization.split(" ")[1]
+        const decoded = jwt.verify(token, process.env.KEY)
+        req.team_id = decoded.team_id
+        next()
+    }
+    catch(err){
+        res.status(401).json({
+            message:'Auth Failed',
+            error:err
+        })
+    }
+}
