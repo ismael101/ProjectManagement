744d2ca176660928e45d2091af30f9d3f400760a
ismael101 <ismaelomermohamed@gmail.com>
2019-12-12 01:11:38 -0600

    added middleware to secure the routes

--- routes/teams.js
+++ routes/teams.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const teamsController = require('../controllers/teams')
+const auth = require('../middleware/auth')
 const router = express.Router()
 
-router.get('/',teamsController.getTeams)
+router.get('/',auth,teamsController.getTeams)
 
-router.get('/:id',teamsController.getTeam)
+router.get('/:id',auth,teamsController.getTeam)
 
-router.post('/',teamsController.createTeam)
+router.post('/',auth,teamsController.createTeam)
 
-router.patch('/:id', teamsController.updateTeam)
+router.patch('/:id',auth, teamsController.updateTeam)
 
-router.delete('/:id', teamsController.deleteTeam)
+router.delete('/:id',auth,teamsController.deleteTeam)
 
 module.exports = router
