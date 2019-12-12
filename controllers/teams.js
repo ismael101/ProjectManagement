1d5ad285702dfde9548b227fe782be7ffc39bb5f
ismael101 <ismaelomermohamed@gmail.com>
2019-12-12 04:38:23 -0600

    added comments to teams controlller and user models

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,14 +1,7 @@
 const Teams = require('../models/').Teams
-const Users = require('../models/').Users
-exports.getMembers = (req,res,next) => {
-    Users.findAll({where:{team_id:req.team_id},attributes: ['id', 'username']})
-         .then(users => {
-            res.status(200).json(users)
-         })
-         .catch(error => {
-            res.status(400).json(error)
-         })
-}
+
+
+//this is a controller that allows authenticaticated the users to fetch the team name and id
 exports.getTeam = (req,res,next) => {
     Teams.findOne({where:{id:req.team_id}})
          .then(team => {
@@ -19,12 +12,16 @@ exports.getTeam = (req,res,next) => {
          })
 }
 
+//this controller lets user create a team
 exports.createTeam = (req,res,next) => {
+    //the team is created here
     Teams.create(req.body)
          .then(() => {
+             //if its a sucess than the user gets this message
             res.status(200).json({message:'Team Created'})
          })
          .catch(error => {
+             //is there is a validation error then its raised here
             if (error instanceof Sequelize.ValidationError) {
                 let messages = error.errors.map( (e) => e.message)
                 return res.status(400).json(messages)
@@ -34,13 +31,17 @@ exports.createTeam = (req,res,next) => {
          })
 }
 
+//this controller let user update there team
 exports.updateTeam = (req,res,next) => {
+    //find the team to check if thes user is apart of the team
     Teams.findOne({where:{id:req.team_id}})
          .then(() => {
+             //updaed the team here
             Teams.update(req.body,{where:{id:req.team_id}})
                 .then(() => {
                     res.status(200).json({message:'Team Updated'})
                 })
+                //if there is a validation error its raised here
                 .catch(error => {
                     if (error instanceof Sequelize.ValidationError) {
                         let messages = error.errors.map( (e) => e.message)
@@ -51,14 +52,18 @@ exports.updateTeam = (req,res,next) => {
                 })
 
          })
+         //if there arent any teams that the user is in 400 bad request is raised
          .catch(error => {
              res.status(400).json(error)
          })
 }
 
+//this controler lets users delete teams
 exports.deleteTeam = (req,res,next) => {
+    //find the team to make the user is in the team
     Teams.findOne({where:{id:req.team_id}})
          .then(() => {
+             //if the user is in the team delete it here
             Teams.delete({where:{team_id:req.team_id}})
                  .then(() => {
                     res.status(200).json({message:'Team Deleted'})
@@ -67,6 +72,7 @@ exports.deleteTeam = (req,res,next) => {
                      res.status(400).json(error)
                  })
          })
+         // if the user isnt in the team the 400 bad request error reaised here
          .catch(error => {
              res.status(400).json(error)
          })
