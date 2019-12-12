6ff8952eb19a8e072ab9d4d10cfa306c1f0c9448
ismael101 <ismaelomermohamed@gmail.com>
2019-12-11 22:10:40 -0600

    created seed file

---
+++ seeders/20191212021440-Projects.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Projects',[
+      {name:"Train Luke",description:"We need to train luke on the ways of the jedi",status:true,team_id:1,createdAt: new Date(), updatedAt: new Date()},
+      {name:"Defeat Darth Vader",description:"The jedi need to defeat darth vader",status:false,team_id:1,createdAt: new Date(), updatedAt: new Date()},
+      {name:"Convert anakin",description:"Convert anakin skywalker to the dark side",status:true,team_id:2,createdAt: new Date(), updatedAt: new Date()},
+      {name:"Conquer the galaxy",description:"Conquer the entire galaxy",status:false,team_id:2,createdAt: new Date(), updatedAt: new Date()},
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   return queryInterface.bulkDelete('Projects', null, {})
+  }
+};
---
+++ seeders/20191212021444-Tasks.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('Tasks',[
+     {name:'Force Training',description:'Train on how to use the force',status:true,user_id:1,project_id:1,team_id:1,createdAt: new Date(), updatedAt: new Date()},
+     {name:'Lightsaber training',description:'Teach luke skywalker how to use a lightsaber',status:true,user_id:2,project_id:1,team_id:1,createdAt: new Date(), updatedAt: new Date()},
+
+     {name:'Go to Cloud City',description:'Go to cloud city to confront darth vader',status:false,user_id:1,project_id:2,team_id:1,createdAt: new Date(), updatedAt: new Date()},
+     {name:'Find out a secret',description:'Find out the secret of who your dad is',status:false,user_id:1,project_id:2,team_id:1,createdAt: new Date(), updatedAt: new Date()},
+
+     {name:'Befriend Anakin',description:'Befriend anakin and slowly pull him in to the dark side',status:true,user_id:4,project_id:3,team_id:2,createdAt: new Date(), updatedAt: new Date()},
+     {name:'Fight with obiwan',description:'Fight with obiwan kenobe on lava planet',status:true,user_id:3,project_id:3,team_id:2,createdAt: new Date(), updatedAt: new Date()},
+
+     {name:'Capture Leia',description:'capture princess leia',status:false,user_id:3,project_id:4,team_id:2,createdAt: new Date(), updatedAt: new Date()},
+     {name:'Destroy Alderaan',description:'Use the death star to destroy the planet alderaan',status:false,user_id:4,project_id:4, team_id:2,createdAt: new Date(), updatedAt: new Date()},
+   ])
+  },
+
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   return queryInterface.bulkDelete('Tasks', null, {})
+  }
+};
