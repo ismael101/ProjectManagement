bd71584eafd4f51b90bc8db95f2e5725f45de270
ismael101 <ismaelomermohamed@gmail.com>
2019-12-13 20:34:38 -0600

    added client folder

---
+++ client/src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class='login'>
+    <v-container>
+      <v-layout column align-center justify-center>
+        <v-flex>
+          <v-alert v-if='error' type='error'>Authentication Error</v-alert>
+          <h1 class='subheading grey--text my-10 align-center my-5'>Login</h1>
+          <v-card width="600" flat>
+            <v-form  ref="form" v-model="valid" lazy-validation >
+              <v-text-field v-model='form.username' required outlined :rules='form.usernameRules' label='Enter Username'/>
+              <v-text-field v-model="form.password" required outlined :rules='form.passwordRules' label='Enter Password'/>
+              <v-btn outlined @click="submit" :disabled="!valid">
+                Login
+              </v-btn>
+            </v-form>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import jsonwebtoken from 'jsonwebtoken'
+
+export default {
+  data(){
+    return{
+      valid: true,
+      form:{
+        username:'',
+        usernameRules:[
+          (v) => !!v || 'Username is Required'],
+        password:'',
+        passwordRules:[
+         (v) => !!v || 'Password is Required'] 
+      },
+      error:false
+    }
+  },
+  methods:{
+    submit(){
+      this.$users.login(this.form)
+                .then(res => {
+                  this.error = false
+                  this.$store.dispatch('setToken', res.token)
+                  let info = jsonwebtoken.decode(res.token)
+                  this.$store.dispatch('setUser', {username:info.username,id:info.id,teamid:info.teamid})
+                  this.$router.push({name:'projects'})
+                })
+                .catch(() => {
+                  this.error = true
+                  this.form.password = ''
+                  this.form.username = ''
+                  this.valid = true
+                })
+    }
+  }
+}
+</script>
+
+<style>
+.login{
+  margin: auto;
+  width: 50%;
+  padding-top: 25vh;
+}
+</style>
