6b91e6cee5c5fcc66a7ea03dab7413dbb3eeac2a
ismael101 <ismaelomermohamed@gmail.com>
2020-04-14 20:27:26 -0500

    env port

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.use(function (err, req, res, next) {
 })
 
 // Start server running 
-var server = app.listen(4000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('app running on port', server.address().port)
 })
